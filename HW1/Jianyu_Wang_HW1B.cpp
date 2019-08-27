eb0092013bb0e32b29fb35b3d5fddf400bc55a61
wjy <wjy@wjydeMBP.fios-router.home>
2019-08-26 21:51:50 -0400

    first commit

---
+++ HW1/Jianyu_Wang_HW1B.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int inp=1000000;
+    double r=1;
+    cin>>inp;
+    for(int n=3;n<inp;n++){
+        r=(r/cos(M_PI/n));
+        if(n==10||n==100||n==1000||n==10000||n==100000||n==1000000)
+            cout<<n<<"    "<<r<<endl;
+    }
+    
+    return 0;
+}
