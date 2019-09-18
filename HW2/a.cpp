d51118f5ad2bb11504e3094f17ba27098510b2ef
37wjy <37wjy@sina.com>
2019-09-17 22:36:36 -0400

    commit

---
+++ HW2/a.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char** argv) {
+    cout<<sizeof(long)<<endl
+    <<sizeof(long long)<<endl
+    <<sizeof(int)<<endl
+    <<sizeof(long double)<<endl
+    <<sizeof(bool)<<endl;
+    return 0;
+}
